98abde69a1fd9bb38a90a6c897f97604f5619cce
Markus Schneider <markus.schneider89@googlemail.com>
2020-08-09 01:47:22 +0200

    add select and cut modes

--- src/main.cpp
+++ src/main.cpp
@@ -90,6 +90,81 @@ char str2char(string s){
   return s[0];
 }
 
+void run_select(string csv_path,
+		string column,
+		string regex,
+		string match,
+		bool complete_match,
+		char delimiter,
+		vector<string> out_columns,
+		size_t read_size,
+		size_t buffer_size){
+    string pattern;
+    string matcher_type;
+    
+    if(!regex.empty()) {
+      matcher_type = "regex";
+      pattern = normalize_regex(regex, delimiter);
+    }
+    else if(!match.empty()){
+      matcher_type = "bm";
+      pattern = match;
+      complete_match = true;
+    }
+    else throw runtime_error("Could not determine matcher type");
+    
+    Context c = create_context(csv_path, read_size, buffer_size);
+    circbuf* cbuf = c.cbuf();
+    linescan* lscan = c.lscan();
+    Line_Scan sc_result;
+
+    scan_header(cbuf, delimiter, lscan, sc_result);
+
+    shared_ptr<Matcher> matcher = create_matcher(pattern, matcher_type, delimiter);
+    shared_ptr<Line_Scan_Printer> printer = create_printer(sc_result, delimiter, out_columns);
+
+    printer->print(sc_result);
+
+    size_t col = column_index(sc_result, column);
+
+    char* head = circbuf_head_forward(cbuf, sc_result.length());
+    while(head[0] != '\0'){
+      scan_match_print_line(cbuf,delimiter,*matcher,col,
+                            complete_match,lscan,sc_result,*printer);
+      head = circbuf_head(cbuf);
+    }
+
+    return;
+}
+
+void run_cut(string csv_path,
+	     char delimiter,
+	     vector<string> out_columns,
+	     size_t read_size,
+	     size_t buffer_size){
+  Context c = create_context(csv_path, read_size, buffer_size);
+  circbuf* cbuf = c.cbuf();
+  linescan* lscan = c.lscan();
+  Line_Scan sc_result;
+
+  scan_header(cbuf, delimiter, lscan, sc_result);
+
+  shared_ptr<Line_Scan_Printer> printer = create_printer(sc_result, delimiter, out_columns);
+  printer->print(sc_result);
+
+  char* head = circbuf_head_forward(cbuf, sc_result.length());
+  while(head[0] != '\0'){
+    char* del = simple_scan_right(head,read_size,delimiter);
+    if(del==nullptr) throw runtime_error("Could not find delimiter in line");
+    scan(head,read_size,NL,delimiter,lscan,sc_result);
+    printer->print(sc_result);
+    head = circbuf_head_forward(cbuf, sc_result.length());
+  }
+
+  return;
+}
+
+
 int main(int argc, const char* argv[]){
   try{
 
@@ -106,63 +181,46 @@ int main(int argc, const char* argv[]){
     string delimiter_str = ",";
     vector<string> out_columns;
     bool complete_match = false;
-    string matcher_type = "";
 
-    app.add_option("-c,--column",column,"Column to match")->required();
     app.add_option("-d,--delimiter",delimiter_str,
 		   "Column delimiter (default '" + delimiter_str + "')");
-    app.add_option("-o,--out-columns",out_columns,"Output columns,separated by ',' (default all)")
-      ->delimiter(',');
     app.add_option("--read-size",read_size,"Size of sequential buffer reads (default 4kb)")
       ->transform(CLI::AsSizeValue(false));
-    app.add_flag("--complete",complete_match,"Require that fields match entirely (always active for --match)");
-    app.add_option("csv",csv_path,"CSV path");
 
-    auto input_optg = app.add_option_group("match")->required();
-        
+    auto select_cmd = app.add_subcommand("select");
+    select_cmd->add_option("-c,--column",column,"Column to match")->required();
+    select_cmd->add_option("-o,--out-columns",out_columns,
+			  "Output columns,separated by ',' (default all)")->delimiter(',');
+    select_cmd->add_flag("--complete",complete_match,"Require that fields match entirely (always active for --match)");
+    select_cmd->add_option("csv",csv_path,"CSV path");
+
+    auto input_optg = select_cmd->add_option_group("match")->required();
     auto regex_opt =
       input_optg->add_option("-r,--regex",regex,"Regex to match in selected column");
     auto match_opt =
       input_optg->add_option("-m,--match",match,"Exact string to match in selected column");
     regex_opt->excludes(match_opt);
     match_opt->excludes(regex_opt);
-   
+
+    auto cut_cmd = app.add_subcommand("cut");
+    cut_cmd->add_option("-c,--columns",out_columns,
+			  "Output columns,separated by ',' (default all)")->delimiter(',');
+    cut_cmd->add_option("csv",csv_path,"CSV path");
+
+    app.require_subcommand(1);
     CLI11_PARSE(app, argc, argv);
 
     uint buffer_size = read_size * 1000;
     char delimiter = str2char(delimiter_str);
-    string pattern;
-    
-    if(!regex.empty()) {
-      matcher_type = "regex";
-      pattern = normalize_regex(regex, delimiter);
-    }
-    else if(!match.empty()){
-      matcher_type = "bm";
-      pattern = match;
-      complete_match = true;
-    }
-    else throw runtime_error("Could not determine matcher type");
-    
-    Context c = create_context(csv_path, read_size, buffer_size);
-    circbuf* cbuf = c.cbuf();
-    linescan* linescan = c.lscan();
-    Line_Scan sc_result;
 
-    scan_header(cbuf, delimiter, linescan, sc_result);
-
-    shared_ptr<Matcher> matcher = create_matcher(pattern, matcher_type, delimiter);
-    shared_ptr<Line_Scan_Printer> printer = create_printer(sc_result, delimiter, out_columns);
-
-    printer->print(sc_result);
-
-    size_t col = column_index(sc_result, column);
-      
-    char* head = circbuf_head_forward(cbuf, sc_result.length());
-    while(head[0] != '\0'){
-      scan_match_print_line(cbuf,delimiter,*matcher,col,
-			    complete_match,linescan,sc_result,*printer);
-      head = circbuf_head(cbuf);
+    if(select_cmd->parsed()){
+      run_select(csv_path, column, regex, match,
+		 complete_match, delimiter,
+		 out_columns, read_size, buffer_size);
+    } else if(cut_cmd->parsed()){
+      run_cut(csv_path, delimiter, out_columns, read_size, buffer_size);
+    } else {
+      throw runtime_error("Unknown subcommand");
     }
     
     fflush(stdout);
